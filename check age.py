dd16008193b1e255e41d51b1ad78fd42305302ad
SubahaSalman <subaha.salman@gmail.com>
2025-07-10 18:15:51 +0200

    age check

---
+++ check age.py
@@ -0,0 +1,10 @@
+age = int(input("Please enter your age: "))
+
+if age>=10:
+    print("You are between 10 and 20 years old!")
+elif age<=20:
+    print("You are between 10 and 20 years old!")
+elif age<=10:
+    print("You are a child under 10.")
+else:
+    print("You are above the age of 20")
